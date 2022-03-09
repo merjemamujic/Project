37365bd16d1d108141279b28b12faf3c77f7bfb7
merjemamujic <merjema.mujic@stu.ibu.edu.ba>
2022-03-09 10:20:23 +0100

    nesto

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php'
+require 'vendor/autoload.php';
 
 Flight::route('/', function(){
     echo 'Hello world!';
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitc16e803f5732d897cded0657fba9ef23
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc16e803f5732d897cded0657fba9ef23', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitc16e803f5732d897cded0657fba9ef23', 'loadClassLoader'));
@@ -50,6 +52,29 @@ class ComposerAutoloaderInitc16e803f5732d897cded0657fba9ef23
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitc16e803f5732d897cded0657fba9ef23::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequirec16e803f5732d897cded0657fba9ef23($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequirec16e803f5732d897cded0657fba9ef23($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
