063014fc9aa7c932a7e2eeb2f43f2f723f8bc4fb
merjemamujic <merjema.mujic@stu.ibu.edu.ba>
2022-03-09 10:09:16 +0100

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1cf41e85543b18b45631a639a628cc527deda04d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1cf41e85543b18b45631a639a628cc527deda04d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
